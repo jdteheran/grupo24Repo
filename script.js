e89e2cc06b84df93d52cd9e9f8de90d618fa11f0
Juan Teheran <jdteheran@uninorte.edu.co>
2021-11-15 14:31:26 -0500

    Codigo casi terminasdo

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hola mundo, grupo de programacion 24');
+
+let numero = 8
+
+function esPrimo(num) {
+    let cont = 0
+    for (let i = 1; i <= num; i++) {
+        if (num%i == 0) {
+            cont = cont + 1
+            //cont++
+        }
+    }
+    
+}
+
