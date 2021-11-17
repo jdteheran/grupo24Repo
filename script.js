1547627ffdae72bd6051a90828bd1dd26e615135
Juan Teheran <jdteheran@uninorte.edu.co>
2021-11-17 12:34:15 -0500

    commit desde dev2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let numero = 8
 function esPrimo(num) {
     let cont = 0
     for (let i = 1; i <= num; i++) {
-        if (num%i == 0) {
+        if (num%i == 0 && true) {
             cont = cont + 1
             //cont++
         }
