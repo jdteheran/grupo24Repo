f59e7dd434777c384f00a72fd380698095f1a1e5
Juan Teheran <jdteheran@uninorte.edu.co>
2021-11-17 12:39:13 -0500

    cuarto commit en master

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log(esPrimo(numero));
 function esPrimo(num) {
     let cont = 0
     for (let i = 1; i <= num; i++) {
-        if (num%i == 0) {
+        if (num % i == 0 && true && true) {
             cont = cont + 1
             //cont++
         }
